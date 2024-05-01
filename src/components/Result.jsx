45154884f3cabbc59a160dc58717906c47fbbb36
Atakanzer6 <atakanoze0770@gmail.com>
2024-05-01 20:17:09 +0300

    add card component

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,15 +1,26 @@
 import Navbar from "./home/Navbar"
 import Search from "./home/Search"
+import Card from "./Card";
 import {useParams} from 'react-router-dom';
 
 export default function Result() {
     const {q} = useParams()
-    console.log(q);
     return (
         <>
             <Navbar />
             <Search />
-            <h1 className="text-green-500">{q}</h1>
+            <hr />
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-6 mx-5 ">
+                {/* grid test */}
+                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
+                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
+                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
+                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
+                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
+                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
+                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
+                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
+            </div>
         </>
     )
 }
