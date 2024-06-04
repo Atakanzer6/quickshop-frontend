aaec0bcf3ace73a3ca0fb58f3f65fc36350397e5
Atakanzer6 <atakanoze0770@gmail.com>
2024-06-04 13:50:54 +0300

    API call & img fix

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,25 +2,52 @@ import Navbar from "./home/Navbar"
 import Search from "./home/Search"
 import Card from "./Card";
 import {useParams} from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Result() {
     const {q} = useParams()
+    const [apiResults, setApiResults] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] =  useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://127.0.0.1:8000/products/${q}`)
+                if (!response.ok) {
+                    throw new Error("Network response failed!")
+                }
+                const data = await response.json();
+                setApiResults(data)
+            } catch (error) {
+                setError(error.message)
+            } finally {
+                setLoading(false)
+            }
+        } 
+
+        fetchData();
+    }, [q])
+
+    console.log(apiResults);
+
     return (
         <>
             <Navbar />
             <Search />
             <hr />
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-6 mx-5 ">
-                {/* grid test */}
-                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
-                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
-                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
-                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
-                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
-                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
-                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
-                <Card name={q} img="https://www.kindacode.com/wp-content/uploads/2022/07/kindacode-example.png" link="https://www.kindacode.com" price="69"/>
-            </div>
+            {loading ? (
+                <p>Loading...</p>
+            ) : error ? (
+                <p>Error: {error}</p>
+            ) : (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-6 mx-5 ">
+                    {apiResults.map(data => (
+                        <Card key={uuidv4()} name={data.newegg.name} link={data.newegg.link} img={data.newegg.image} price={data.newegg.price} />
+                    ))}
+                </div>
+            )}
         </>
     )
 }
--- src/components/home/Search.jsx
+++ src/components/home/Search.jsx
@@ -9,16 +9,17 @@ export default function Search() {
         setSearchQuery(event.target.value)
     }
 
-    const handleClick = () => {
-        const formattedQuery = searchQuery.trim().replace(/\s+/g, '-');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formattedQuery = searchQuery.trim().replace(/\s+/g, '+');
         navigate(`/search/${formattedQuery}`)
     }
 
         
     return (
-        <form onSubmit={handleClick} className="flex justify-center py-20">
+        <form onSubmit={handleSubmit} className="flex justify-center py-20">
             <input type="text" required value={searchQuery}  onChange={handleQuery} placeholder="Search for a product..." className="border border-green-500 focus:outline-none focus:ring-green-500 focus:border-green-500 "/>
-            <button  className="bg-green-500 px-4 py-2 ">Search</button>
+            <button type="submit" className="bg-green-500 px-4 py-2 ">Search</button>
         </form>
     )
 }
