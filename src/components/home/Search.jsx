4708f47d51dc0bd8ff9a96eda28c0b05629cc461
Atakanzer6 <atakanoze0770@gmail.com>
2024-07-03 16:27:47 +0300

    restyle card component

--- src/components/home/Search.jsx
+++ src/components/home/Search.jsx
@@ -19,7 +19,7 @@ export default function Search() {
     return (
         <form onSubmit={handleSubmit} className="flex justify-center py-2 ">
             <input type="text" required value={searchQuery}  onChange={handleQuery} placeholder="Search for a product..." className="border rounded-bl-xl rounded-tl-xl border-green-500 focus:outline-none focus:ring-green-500 focus:border-green-500 "/>
-            <button type="submit" className="bg-green-500 px-4 py-2 rounded-tr-xl rounded-br-xl">Search</button>
+            <button type="submit" className="bg-green-500 px-4 py-2 rounded-tr-xl rounded-br-xl"><span className="text-white">Search</span></button>
         </form>
     )
 }
--- src/components/searchpage/Card.jsx
+++ src/components/searchpage/Card.jsx
@@ -1,11 +1,26 @@
-export default function Card({img, link, price, name}) {
-    return (
-        <a href={link} className="flex flex-col bg-white drop-shadow hover:drop-shadow-lg hover:opacity-70  my-2 mx-2 max-w-xs">
-            <img src={img} alt="product image" className="h-36 object-scale-down rounded-tl-md rounded-tr-md" />
-            <div className="px-3 py-2">
-                <h3 className="font-semibold">{name}</h3>
-                <p className="text-sm">${price}</p>
-            </div>
-        </a>
-    )
+export default function Card({ img, link, price, name }) {
+  return (
+    <div className="flex flex-col bg-white drop-shadow hover:drop-shadow-lg   my-2 mx-2 max-w-xs">
+      <div className="relative self-center ">
+        <img
+          className="h-36 object-scale-down rounded-tl-md rounded-tr-md"
+          src={img}
+          alt="Product Image"
+        />
+      </div>
+      <hr className="mb-auto" />
+      <div className="p-4">
+        {/* <h3 className="text-lg font-medium mb-2">Product Title</h3> */}
+        <p className="text-gray-600 text-sm mb-4">{name}</p>
+        <div className="flex items-center justify-between ">
+          <span className="font-bold text-lg">${price}</span>
+          <a href={link}>
+            <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
+              Buy Now
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
